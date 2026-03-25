0cb79c248537ca4dde71be197b4adae33dd1ddf9
kendal125 <klopez80241@ufide.ac.cr>
2026-03-24 20:06:40 -0600

    arreglo

--- Proyecto_BusNova/src/main/java/com/mycompany/proyecto_busnova/Tiquete.java
+++ Proyecto_BusNova/src/main/java/com/mycompany/proyecto_busnova/Tiquete.java
@@ -45,13 +45,10 @@ public class Tiquete {
      * @param terminalOrigen terminal donde se compró el tiquete
      */
 
-<<<<<<< HEAD
     
     public Tiquete() {
     }
-
-=======
->>>>>>> 5a672067987d1d77d0f48d7c06bcf5adc569674e
+     
     public Tiquete(String id, TipoServicio tipoServicio, double pesoCarga, Date horaCreacion, Date horaAtencion, Estado estado, int busAsignadoId, String terminalOrigen, double precioCalculado) {
         this.id = id;
         this.tipoServicio = tipoServicio;
